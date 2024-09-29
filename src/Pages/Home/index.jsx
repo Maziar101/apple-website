b8a220f8bc09717dd259adf6c5e03ff5c67e7272
Maziar101 <maziarzamani85@gmail.com>
2024-09-30 00:03:05 +0330

    Finish Apple 16 Banner In Main

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import { Button, Stack, Typography } from "@mui/material";
 import Banner1 from "../../assets/banner1.jpg";
-import Banner2 from "../../assets/banner2.jpg"
+import Banner2 from "../../assets/banner2.jpg";
 import React from "react";
 
 export default function Home() {
@@ -14,9 +14,9 @@ export default function Home() {
             backgroundSize: "cover",
             backgroundPosition: "center",
             height: "692px",
-              textAlign: "center",
-              justifyContent:'space-between',
-            padding:'40px 0'
+            textAlign: "center",
+            justifyContent: "space-between",
+            padding: "40px 0",
           }}
         >
           <Stack
@@ -24,41 +24,68 @@ export default function Home() {
               justifyContent: "start",
             }}
           >
-            <Typography variant="h2" sx={{ color: "white" , letterSpacing:'-.005em' , fontWeight:'600' }}>
+            <Typography
+              variant="h2"
+              sx={{
+                color: "white",
+                letterSpacing: "-.005em",
+                fontWeight: "600",
+              }}
+            >
               iPhone 16 Pro
             </Typography>
-            <Typography variant="p" sx={{ color: "white" , fontSize:'28px' , letterSpacing:'.007em'}}>
+            <Typography
+              variant="p"
+              sx={{ color: "white", fontSize: "28px", letterSpacing: ".007em" }}
+            >
               Hello, Apple Intelligence.
             </Typography>
           </Stack>
-          <Stack sx={{gap:'20px'}}>
+          <Stack sx={{ gap: "20px" }}>
             <Stack
               sx={{
                 flexDirection: "row",
                 margin: "0 auto",
-                gap:'20px'
+                gap: "20px",
               }}
             >
-              <Button sx={{borderRadius:'20px',background:'#0071E3'}} size="large" variant="contained">Learn more</Button>
-              <Button sx={{borderRadius:'20px' , '&:hover':{background:'#0071E3',color:'#fff'}}} size="large" variant="outlined">Buy</Button>
+              <Button
+                sx={{ borderRadius: "20px", background: "#0071E3" }}
+                size="large"
+                variant="contained"
+              >
+                Learn more
+              </Button>
+              <Button
+                sx={{
+                  borderRadius: "20px",
+                  "&:hover": { background: "#0071E3", color: "#fff" },
+                }}
+                size="large"
+                variant="outlined"
+              >
+                Buy
+              </Button>
             </Stack>
             <Stack sx={{ justifyContent: "end" }}>
-              <Typography sx={{color:'#86868b',fontSize:'17px'}}>
-                Apple Intelligence coming this fall <sup style={{fontSize:'12px'}}>2</sup>
+              <Typography sx={{ color: "#86868b", fontSize: "17px" }}>
+                Apple Intelligence coming this fall{" "}
+                <sup style={{ fontSize: "12px" }}>2</sup>
               </Typography>
             </Stack>
           </Stack>
         </Stack>
-        {/* First Advertisement For Apple 16 */}
+        {/* Second Advertisement For Apple 16 */}
         <Stack
           sx={{
             backgroundImage: `url(${Banner2})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             height: "692px",
-              textAlign: "center",
-              justifyContent:'space-between',
-            padding:'40px 0'
+            textAlign: "center",
+            justifyContent: "space-between",
+            padding: "40px 0",
+            marginTop:'10px'
           }}
         >
           <Stack
@@ -66,31 +93,59 @@ export default function Home() {
               justifyContent: "start",
             }}
           >
-            <Typography variant="h2" sx={{ color: "white" , letterSpacing:'-.005em' , fontWeight:'600' }}>
-              iPhone 16 Pro
+            <Typography
+              variant="h2"
+              sx={{
+                color: "white",
+                letterSpacing: "-.005em",
+                fontWeight: "600",
+              }}
+            >
+              iPhone 16
             </Typography>
-            <Typography variant="p" sx={{ color: "white" , fontSize:'28px' , letterSpacing:'.007em'}}>
+            <Typography
+              variant="p"
+              sx={{ color: "white", fontSize: "28px", letterSpacing: ".007em" }}
+            >
               Hello, Apple Intelligence.
             </Typography>
           </Stack>
-          <Stack sx={{gap:'20px'}}>
+          <Stack sx={{ gap: "20px" }}>
             <Stack
               sx={{
                 flexDirection: "row",
                 margin: "0 auto",
-                gap:'20px'
+                gap: "20px",
               }}
             >
-              <Button sx={{borderRadius:'20px',background:'#0071E3'}} size="large" variant="contained">Learn more</Button>
-              <Button sx={{borderRadius:'20px' , '&:hover':{background:'#0071E3',color:'#fff'}}} size="large" variant="outlined">Buy</Button>
+              <Button
+                sx={{ borderRadius: "20px", background: "#0071E3" }}
+                size="large"
+                variant="contained"
+              >
+                Learn more
+              </Button>
+              <Button
+                sx={{
+                  borderRadius: "20px",
+                  "&:hover": { background: "#0071E3", color: "#fff" },
+                }}
+                size="large"
+                variant="outlined"
+              >
+                Buy
+              </Button>
             </Stack>
             <Stack sx={{ justifyContent: "end" }}>
-              <Typography sx={{color:'#86868b',fontSize:'17px'}}>
-                Apple Intelligence coming this fall <sup style={{fontSize:'12px'}}>2</sup>
+              <Typography sx={{ color: "#1d1d1f", fontSize: "19px" }}>
+                Apple Intelligence coming this fall{" "}
+                <sup style={{ fontSize: "12px" }}>3</sup>
               </Typography>
             </Stack>
           </Stack>
         </Stack>
+        {/* Second Advertisement For Apple 16 */}
+
       </Stack>
     </>
   );
