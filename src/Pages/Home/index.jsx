f15af7563f0124fe213771f2abf8986e842ad4cb
Maziar101 <maziarzamani85@gmail.com>
2024-09-30 00:57:16 +0330

    Finish Apple Watch Series 10 In Main

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import { Button, Stack, Typography } from "@mui/material";
 import Banner1 from "../../assets/banner1.jpg";
 import Banner2 from "../../assets/banner2.jpg";
+import Logo from "../../assets/download.svg";
+import AppleWatchBanner from "../../assets/apple-watch-banner.jpg";
 import React from "react";
 
 export default function Home() {
@@ -50,7 +52,7 @@ export default function Home() {
               }}
             >
               <Button
-                sx={{ borderRadius: "20px", background: "#0071E3" }}
+                sx={{ borderRadius: "20px", background: "#0071E3",textTransform: 'none',}}
                 size="large"
                 variant="contained"
               >
@@ -58,7 +60,7 @@ export default function Home() {
               </Button>
               <Button
                 sx={{
-                  borderRadius: "20px",
+                  borderRadius: "20px",textTransform: 'none',
                   "&:hover": { background: "#0071E3", color: "#fff" },
                 }}
                 size="large"
@@ -85,7 +87,7 @@ export default function Home() {
             textAlign: "center",
             justifyContent: "space-between",
             padding: "40px 0",
-            marginTop:'10px'
+            marginTop: "10px",
           }}
         >
           <Stack
@@ -119,7 +121,7 @@ export default function Home() {
               }}
             >
               <Button
-                sx={{ borderRadius: "20px", background: "#0071E3" }}
+                sx={{ borderRadius: "20px", background: "#0071E3" ,textTransform: 'none',}}
                 size="large"
                 variant="contained"
               >
@@ -127,7 +129,7 @@ export default function Home() {
               </Button>
               <Button
                 sx={{
-                  borderRadius: "20px",
+                  borderRadius: "20px",textTransform: 'none',
                   "&:hover": { background: "#0071E3", color: "#fff" },
                 }}
                 size="large"
@@ -144,8 +146,78 @@ export default function Home() {
             </Stack>
           </Stack>
         </Stack>
-        {/* Second Advertisement For Apple 16 */}
-
+        {/* Third Advertisement For Apple Watch Series 10 */}
+        <Stack
+          sx={{
+            backgroundImage: `url(${AppleWatchBanner})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "692px",
+            textAlign: "center",
+            padding: "40px 0",
+            gap: "15px",
+            marginTop: "10px",
+          }}
+        >
+          <Stack
+            sx={{
+              gap:'15px'
+            }}
+          >
+            <Typography
+              variant="h2"
+              sx={{
+                letterSpacing: "-.005em",
+                fontWeight: "600",
+                display: "flex",
+                fontSize: "38px",
+                height:'30px',
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "10px",
+              }}
+            >
+              <img src={Logo} alt="" style={{ width: "38px" }} />
+              WATCH
+            </Typography>
+            <Typography sx={{fontSize:'19px',letterSpacing:'3px'}}>
+              SERIES 10
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{ fontSize: "28px", letterSpacing: ".007em" , marginTop:'-10px' }}
+            >
+              Thinstant classic.
+            </Typography>
+          </Stack>
+          <Stack sx={{ gap: "15px" }}>
+            <Stack
+              sx={{
+                flexDirection: "row",
+                margin: "0 auto",
+                gap: "20px",
+              }}
+            >
+              <Button
+                sx={{ borderRadius: "20px", background: "#0071E3", textTransform: 'none', }}
+                size="large"
+                variant="contained"
+              >
+                Learn more
+              </Button>
+              <Button
+                sx={{
+                  borderRadius: "20px",textTransform: 'none',
+                  "&:hover": { background: "#0071E3", color: "#fff" },
+                }}
+                size="large"
+                variant="outlined"
+              >
+                Buy
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
       </Stack>
     </>
   );
