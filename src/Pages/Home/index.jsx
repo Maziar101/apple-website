aee13e2aa39cc460d65ed696dfde084f71e3c8cb
Maziar101 <maziarzamani85@gmail.com>
2024-09-29 23:59:47 +0330

    Complete Fist Banner Of Main

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import { Button, Stack, Typography } from "@mui/material";
+import Banner1 from "../../assets/banner1.jpg";
+import Banner2 from "../../assets/banner2.jpg"
+import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Stack component={"main"}>
+        {/* First Advertisement For Apple 16 Pro*/}
+        <Stack
+          sx={{
+            backgroundImage: `url(${Banner1})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "692px",
+              textAlign: "center",
+              justifyContent:'space-between',
+            padding:'40px 0'
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "start",
+            }}
+          >
+            <Typography variant="h2" sx={{ color: "white" , letterSpacing:'-.005em' , fontWeight:'600' }}>
+              iPhone 16 Pro
+            </Typography>
+            <Typography variant="p" sx={{ color: "white" , fontSize:'28px' , letterSpacing:'.007em'}}>
+              Hello, Apple Intelligence.
+            </Typography>
+          </Stack>
+          <Stack sx={{gap:'20px'}}>
+            <Stack
+              sx={{
+                flexDirection: "row",
+                margin: "0 auto",
+                gap:'20px'
+              }}
+            >
+              <Button sx={{borderRadius:'20px',background:'#0071E3'}} size="large" variant="contained">Learn more</Button>
+              <Button sx={{borderRadius:'20px' , '&:hover':{background:'#0071E3',color:'#fff'}}} size="large" variant="outlined">Buy</Button>
+            </Stack>
+            <Stack sx={{ justifyContent: "end" }}>
+              <Typography sx={{color:'#86868b',fontSize:'17px'}}>
+                Apple Intelligence coming this fall <sup style={{fontSize:'12px'}}>2</sup>
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+        {/* First Advertisement For Apple 16 */}
+        <Stack
+          sx={{
+            backgroundImage: `url(${Banner2})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "692px",
+              textAlign: "center",
+              justifyContent:'space-between',
+            padding:'40px 0'
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "start",
+            }}
+          >
+            <Typography variant="h2" sx={{ color: "white" , letterSpacing:'-.005em' , fontWeight:'600' }}>
+              iPhone 16 Pro
+            </Typography>
+            <Typography variant="p" sx={{ color: "white" , fontSize:'28px' , letterSpacing:'.007em'}}>
+              Hello, Apple Intelligence.
+            </Typography>
+          </Stack>
+          <Stack sx={{gap:'20px'}}>
+            <Stack
+              sx={{
+                flexDirection: "row",
+                margin: "0 auto",
+                gap:'20px'
+              }}
+            >
+              <Button sx={{borderRadius:'20px',background:'#0071E3'}} size="large" variant="contained">Learn more</Button>
+              <Button sx={{borderRadius:'20px' , '&:hover':{background:'#0071E3',color:'#fff'}}} size="large" variant="outlined">Buy</Button>
+            </Stack>
+            <Stack sx={{ justifyContent: "end" }}>
+              <Typography sx={{color:'#86868b',fontSize:'17px'}}>
+                Apple Intelligence coming this fall <sup style={{fontSize:'12px'}}>2</sup>
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </>
+  );
 }
